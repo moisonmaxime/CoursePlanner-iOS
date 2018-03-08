977aa35fea453016c0c2b6eb5c245bdc282d0ca2
Maxime Moison <maximaxof@me.com>
2018-03-07 20:03:46 -0800

    Initial Commit

---
+++ CoursePlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoursePlanner
+//
+//  Created by Maxime Moison on 3/7/18.
+//  Copyright © 2018 Maxime Moison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
