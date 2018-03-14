e3b04df3ee52337fd8288c881b250bbc5b743d32
Maxime Moison <maximaxof@me.com>
2018-03-14 12:40:22 -0700

    Added views and VCs for Home, Login, Register

---
+++ CoursePlanner/HomeVC.swift
@@ -0,0 +1,35 @@
+//
+//  HomeVC.swift
+//  CoursePlanner
+//
+//  Created by Maxime Moison on 3/14/18.
+//  Copyright © 2018 Maxime Moison. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CoursePlanner/LoginVC.swift
@@ -0,0 +1,47 @@
+//
+//  LoginVC.swift
+//  CoursePlanner
+//
+//  Created by Maxime Moison on 3/14/18.
+//  Copyright © 2018 Maxime Moison. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+        @IBOutlet weak var userField: UITextField!
+        @IBOutlet weak var passwordField: UITextField!
+        @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        @IBAction func loginClick(_ sender: Any) {
+            activityIndicator.startAnimating()
+            self.view.isUserInteractionEnabled = false
+            RestAPI.login(user: userField.text!, password: passwordField.text!) { (isSuccess, error) in
+                
+                DispatchQueue.main.async {
+                    self.view.isUserInteractionEnabled = true
+                    self.activityIndicator.stopAnimating()
+                }
+                
+                if error != nil {
+                    var msg:String
+                    
+                    print("Error")
+                    return
+                }
+                
+                print(isSuccess ? "Successful" : "Failed")
+            }
+        }
+        
+}
