8645106a5ad5a644a8d530619c26aa164b9f8267
Maxime Moison <maximaxof@me.com>
2018-03-15 09:22:29 -0700

    Fix indentation

--- CoursePlanner/LoginVC.swift
+++ CoursePlanner/LoginVC.swift
@@ -9,37 +9,38 @@
 import UIKit
 
 class LoginVC: UIViewController {
-        @IBOutlet weak var userField: UITextField!
-        @IBOutlet weak var passwordField: UITextField!
-        @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-        }
-        
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-        }
-        
-        @IBAction func loginClick(_ sender: Any) {
-            activityIndicator.startAnimating()
-            self.view.isUserInteractionEnabled = false
-            RestAPI.login(user: userField.text!, password: passwordField.text!) { (isSuccess, error) in
-                DispatchQueue.main.async {
-                    self.view.isUserInteractionEnabled = true
-                    self.activityIndicator.stopAnimating()
-                    if (isSuccess) {
-                        let home = self.storyboard?.instantiateViewController(withIdentifier: "Home")
-                        self.navigationController?.setViewControllers([home!], animated: true)
-                    }
-                }
-                if error != nil {
-                    print("Error")
-                    return
+    @IBOutlet weak var userField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginClick(_ sender: Any) {
+        activityIndicator.startAnimating()
+        self.view.isUserInteractionEnabled = false
+        RestAPI.login(user: userField.text!, password: passwordField.text!) { (isSuccess, error) in
+            DispatchQueue.main.async {
+                self.view.isUserInteractionEnabled = true
+                self.activityIndicator.stopAnimating()
+                if (isSuccess) {
+                    (self.navigationController as! NavigationController).setAnimationType(type: FadingAnimator.self, isRepeating: false)
+                    let home = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.navigationController?.setViewControllers([home!], animated: true)
                 }
             }
+            if error != nil {
+                print("Error")
+                return
+            }
         }
-        
+    }
+    
 }
