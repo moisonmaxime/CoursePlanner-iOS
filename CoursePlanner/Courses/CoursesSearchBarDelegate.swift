6d672db00236c5d0082a074dceaf918cea22a06b
Maxime Moison <maximaxof@me.com>
2018-04-03 21:38:06 -0700

    Fix empty search results

--- CoursePlanner/Courses/CoursesSearchBarDelegate.swift
+++ CoursePlanner/Courses/CoursesSearchBarDelegate.swift
@@ -19,18 +19,21 @@ extension CoursesVC: UISearchBarDelegate {
                         return
                     }
                 }
-                
-                self.searchedCourses = result!.sorted(by: { (c1, c2) -> Bool in
-                    let s1 = c1["name"]!.split(separator: "-")
-                    let s2 = c2["name"]!.split(separator: "-")
-                    
-                    if (s1[0] == s2[0]) {
-                        let n1 = Int(s1[1]) ?? 0
-                        let n2 = Int(s2[1]) ?? 0
-                        return n1 < n2
-                    }
-                    return s1[0] < s2[0]
-                })
+                if (result != nil) {
+                    self.searchedCourses = result!.sorted(by: { (c1, c2) -> Bool in
+                        let s1 = c1["name"]!.split(separator: "-")
+                        let s2 = c2["name"]!.split(separator: "-")
+                        
+                        if (s1[0] == s2[0]) {
+                            let n1 = Int(s1[1]) ?? 0
+                            let n2 = Int(s2[1]) ?? 0
+                            return n1 < n2
+                        }
+                        return s1[0] < s2[0]
+                    })
+                } else {
+                    self.searchedCourses = []
+                }
                 DispatchQueue.main.async {
                     self.searchTable.reloadData()
                 }
