be4a621cfcb7857ca627eab38fc08abab2b53d8d
Maxime Moison <maximaxof@me.com>
2018-04-03 17:36:58 -0700

    removed xcuserdata

---
+++ CoursePlanner/Courses/Sections/SectionsTableDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SectionsTableDelegate.swift
+//  CoursePlanner
+//
+//  Created by Maxime Moison on 4/3/18.
+//  Copyright © 2018 Maxime Moison. All rights reserved.
+//
+
+import Foundation
