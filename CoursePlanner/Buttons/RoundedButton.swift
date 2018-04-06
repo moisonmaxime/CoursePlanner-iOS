642de0d33452c66a69909f3f7634fb5a8486995b
Maxime Moison <maximaxof@me.com>
2018-04-05 23:01:00 -0700

    Update IBInspectable

--- CoursePlanner/Buttons/RoundedButton.swift
+++ CoursePlanner/Buttons/RoundedButton.swift
@@ -10,7 +10,11 @@ import UIKit
 
 @IBDesignable class RoundedButton: UIButton {
     
-    @IBInspectable var cornerRadius:CGFloat = 5.0
+    @IBInspectable var cornerRadius:CGFloat = 5.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
