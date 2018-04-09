4cacab8e3a37a652a9e0a715c95229bdca576a6b
Maxime Moison <maximaxof@me.com>
2018-04-08 17:23:35 -0700

    Latest term on top

--- CoursePlanner/Home/HomeVC.swift
+++ CoursePlanner/Home/HomeVC.swift
@@ -58,9 +58,9 @@ class HomeVC: UIViewController {
                         let t1 = s1.readableTerm().split(separator: " ")
                         let t2 = s2.readableTerm().split(separator: " ")
                         if (t1[1] == t2[1]) {
-                            return t1[0] > t2[0]
+                            return t1[0] < t2[0]
                         }
-                        return t1[1] < t2[1]
+                        return t1[1] > t2[1]
                     })
                     self.termTable.reloadData();
                 }
